b812cc86bd426ca00464a40572f24e0fae294070
mabrowarski <mabrowarski@gmail.com>
2023-10-26 11:02:37 +0200

    Add User entity configuration

---
+++ ShopAPI/DB/EntityConfig/UserEntityTypeConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ShopAPI.Model;
+
+namespace ShopAPI.DB.EntityConfig
+{
+	public class UserEntityTypeConfiguration : IEntityTypeConfiguration<User>
+	{
+		public void Configure(EntityTypeBuilder<User> builder)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
