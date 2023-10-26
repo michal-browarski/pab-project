711e33e517c4ac49c8c4de1eb37e5018363c2cee
mabrowarski <mabrowarski@gmail.com>
2023-10-26 13:19:53 +0200

    Seed database with user related data

--- ShopAPI/DB/EntityConfig/UserEntityTypeConfiguration.cs
+++ ShopAPI/DB/EntityConfig/UserEntityTypeConfiguration.cs
@@ -25,6 +25,65 @@ namespace ShopAPI.DB.EntityConfig
 			builder
 				.Property(u => u.Role)
 				.IsRequired();
+
+			builder
+				.HasData
+				(
+					new User 
+					{
+						Id = 1,
+						Email = "admin_email@email.com",
+						Password = "G@garin12_",
+						CreationDateTime = DateTime.Now,
+						IsActive = true,
+						Role = UserRole.Admin,
+						PersonalInfo = new PersonalInfo
+						{
+							Id = 1,
+							FirstName = "Jan",
+							LastName = "Muzykant",
+							PhoneNumber = "000111222",
+							Gender = Gender.Male
+						},
+						AdressInfo = new AdressInfo
+						{
+							Id = 1,
+							PostalCode = "00-000",
+							PostalCity = "Przykład",
+							Settlement = "Przykład",
+							StreetName = "Przykładowa",
+							StreetNumber = "1",
+							HomeNumber = "2"
+						}
+					},
+					new User
+					{
+						Id = 1,
+						Email = "customer_email.email.com",
+						Password = "G@garin12_",
+						CreationDateTime = DateTime.Now,
+						IsActive = true,
+						Role = UserRole.Customer,
+						PersonalInfo = new PersonalInfo
+						{
+							Id = 2,
+							FirstName = "Maria",
+							LastName = "Wiktorska",
+							PhoneNumber = "111222333",
+							Gender = Gender.Female
+						},
+						AdressInfo = new AdressInfo
+						{
+							Id = 2,
+							PostalCode = "00-000",
+							PostalCity = "Przykładowo",
+							Settlement = "Przykładno",
+							StreetName = "Przykładowa",
+							StreetNumber = "1",
+							HomeNumber = "2"
+						}
+					}
+				);
 		}
 	}
 }
